4fb804ef0b4fe40d40b3b4871d93a01137e3365e
evandrosaturnino <evandro.saturnino@gmail.com>
2022-12-13 17:20:14 -0300

    feat: added multiple stores in useliquitystore

--- packages/lib-react/src/hooks/useLiquityStore.ts
+++ packages/lib-react/src/hooks/useLiquityStore.ts
@@ -2,14 +2,14 @@ import { useContext } from "react";
 
 import { LiquityStore as ThresholdStore} from "@liquity/lib-base";
 
-import {LiquityStoreContext } from "../components/LiquityStoreProvider";
+import { LiquityStoreContext } from "../components/LiquityStoreProvider";
 
-export const useLiquityStore = <T>(): ThresholdStore<T> => {
-  const store = useContext(LiquityStoreContext);
+export const useLiquityStore = <T>(): ThresholdStore<T>[] => {
+  const stores = useContext(LiquityStoreContext);
 
-  if (!store) {
+  if (!stores) {
     throw new Error("You must provide a ThresholdStore via ThresholdStoreProvider");
   }
 
-  return store as ThresholdStore<T>;
+  return stores as ThresholdStore<T>[];
 };
