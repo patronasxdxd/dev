dbdd895ccaa250b7c71c6beb662c089271adcf97
evandrosaturnino <evandro.saturnino@gmail.com>
2023-03-21 17:30:08 -0300

    fix: libethers tests fixed

--- packages/lib-ethers/hardhat.config.ts
+++ packages/lib-ethers/hardhat.config.ts
@@ -173,8 +173,8 @@ extendEnvironment(env => {
   env.deployLiquity = async (
     deployer,
     oracleAddresses,
-    collateralSymbol = "eth",
-    collateralAddress = ZERO_ADDRESS,
+    collateralSymbol = "tst",
+    collateralAddress,
     delay = 90 * 24 * 60 * 60,
     stablecoinAddress = "",
     useRealPriceFeed = false,
--- packages/lib-ethers/utils/constants.ts
+++ packages/lib-ethers/utils/constants.ts
@@ -1,1 +1,2 @@
 export const ZERO_ADDRESS = '0x' + '0'.repeat(40)
+export const GOERLI_TBTC_ADDRESS = "0x679874fbe6d4e7cc54a59e315ff1eb266686a937"
