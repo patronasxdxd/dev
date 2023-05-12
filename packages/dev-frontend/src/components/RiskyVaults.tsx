ae1618e763659d4c7e5e185731b2e6b06ff5cc98
evandrosaturnino <evandro.saturnino@gmail.com>
2023-05-13 00:11:57 +0200

    fix: get chainid from usewalletconnector hook

--- packages/dev-frontend/src/components/RiskyVaults.tsx
+++ packages/dev-frontend/src/components/RiskyVaults.tsx
@@ -21,6 +21,7 @@ import { Icon } from "./Icon";
 import { Transaction } from "./Transaction";
 import { Tooltip } from "./Tooltip";
 import { Abbreviation } from "./Abbreviation";
+import { useWalletConnector } from "../hooks/WalletConnectorContext";
 
 const rowHeight = "40px";
 const pageSize = 10;
@@ -69,8 +70,8 @@ const select = ({
   symbol
 });
 
-export const RiskyVaults = ({ version, collateral, isMintList }: RiskyVaultsProps): JSX.Element => {
-  const { chainId } = useWeb3React<Web3Provider>();
+export const RiskyVaults = ({ version, collateral }: RiskyVaultsProps): JSX.Element => {
+  const { account: {chainId} } = useWalletConnector();
   const thresholdSelectorStores = useThresholdSelector(select);
   const thresholdStore = thresholdSelectorStores.find((store) => {
     return store.version === version && store.collateral === collateral;
