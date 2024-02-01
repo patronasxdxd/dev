b15b9c0c78dcd2fe3d230c52ac14b2552f23f6b5
evandrosaturnino <evandro.saturnino@gmail.com>
2024-02-01 12:15:47 -0300

    refactor: transfer ownership to integrations guild

--- packages/contracts/contracts/UpgradeableContracts/THUSDOwnerUpgradeableProxy.sol
+++ packages/contracts/contracts/UpgradeableContracts/THUSDOwnerUpgradeableProxy.sol
@@ -8,23 +8,28 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract THUSDOwnerUpgradeableProxy is Initializable, CheckContract, OwnableUpgradeable {
-    ITHUSDToken private thusdToken;
+   ITHUSDToken private thusdToken;
 
     address public governorBravoAddress;
 
     function initialize(
         address _governorBravoAddress, 
-        address _thusdTokenAddress
+        address _thusdTokenAddress,
+        address _integrationsGuild
     ) 
         public 
         initializer 
     {
         __Ownable_init();
-
+        
         checkContract(_thusdTokenAddress);
         thusdToken = ITHUSDToken(_thusdTokenAddress);
 
+        require(_integrationsGuild != address(0), "Integrations Guild address must be specified");
+        require(_governorBravoAddress != address(0), "Governor Bravo address must be specified");
+
         governorBravoAddress = _governorBravoAddress;
+        _transferOwnership(_integrationsGuild);
     }
 
     function startRevokeMintList(address _account) external onlyOwner {
